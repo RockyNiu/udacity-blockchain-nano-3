ba2ea128eef68d05986ea5bc97e0e4f3bc4a11ab
Lei Niu <lei.niu.ny@gmail.com>
2023-04-03 22:36:22 -0400

    Fix the issue with app

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # udacity-blockchain-nano-3
 
 Homework for [udacity/nd1309-Project-6b-Example-Template (github.com)](https://github.com/udacity/nd1309-Project-6b-Example-Template/tree/master/project-6)
+
+* UML - [Diagram on Whimsical](https://whimsical.com/tracking-items-through-supply-chain-3QPWh43NUVrkRoJpj64RLR)
+* Libraries
+  * `Truffle` - for local testing and deployment
+    `Truffle` v5.8.0 (core: 5.8.0)
+    `Ganache` v7.7.6
+    `Solidity` - 0.4.24 (solc-js)
+    `Node` v18.14.2
+    `Web3.js` v1.8.2
+  * `@truffle/hdwallet-provider` v2.1.9 - used in `truffle.js` to connect to a public test network
+  * `truffle-assertions` v0.9.2 - used in unit tests, especially for checking emitted events
+  * `truffle-contract` v4.0.31 - used in app 
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,16 @@
 // migrating the appropriate contracts
-var FarmerRole = artifacts.require("./FarmerRole.sol");
-var DistributorRole = artifacts.require("./DistributorRole.sol");
-var RetailerRole = artifacts.require("./RetailerRole.sol");
-var ConsumerRole = artifacts.require("./ConsumerRole.sol");
-var SupplyChain = artifacts.require("./SupplyChain.sol");
+const Roles = artifacts.require("./Roles.sol");
+const FarmerRole = artifacts.require("./FarmerRole.sol");
+const DistributorRole = artifacts.require("./DistributorRole.sol");
+const RetailerRole = artifacts.require("./RetailerRole.sol");
+const ConsumerRole = artifacts.require("./ConsumerRole.sol");
+const SupplyChain = artifacts.require("./SupplyChain.sol");
+const Ownable = artifacts.require("./Ownable.sol");
+
 
 module.exports = function(deployer) {
+  deployer.deploy(Ownable);
+  deployer.deploy(Roles);
   deployer.deploy(FarmerRole);
   deployer.deploy(DistributorRole);
   deployer.deploy(RetailerRole);
